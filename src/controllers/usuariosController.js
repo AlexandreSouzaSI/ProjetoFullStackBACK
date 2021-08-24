275422d093236082c09ea8ee42a0f3fad6f2de4a
Alexandre <alemoura23@hotmail.com>
2021-08-24 18:13:26 -0300

    jwt

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -1,5 +1,8 @@
 const pool = require('../infra/database');
-
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const autorização = require('../auth/jwtauth')
 
 
 const getUsuario = async (req, res) => {
@@ -8,10 +11,11 @@ const getUsuario = async (req, res) => {
 }
 
 const cadastrarUsuario = async (req, res) => {
-    const { name, email, telefone, senha } = req.body;
+    const { name, email, telefone } = req.body;
+    const senhaCrypto = await bcrypt.hash(req.body.senha, 10);
     const response = await pool.query(`INSERT INTO usuarios_cadastrados (name, email, telefone, senha, date_create) 
     VALUES ($1, $2, $3, $4, $5) returning *`, 
-    [name, email, telefone, senha , date_create = new Date()])
+    [name, email, telefone, senha = senhaCrypto , date_create = new Date()])
     res.status(201).json({
         message: 'Usuario cadastrado com sucesso!',
         body: {
@@ -19,6 +23,34 @@ const cadastrarUsuario = async (req, res) => {
         }
     })
 };
+
+const loginUsuario = async (req, res) => {
+    const { email, senha } = req.body;
+    let token;
+    const response = await pool.query('SELECT id_usuario_cadastrado, email, senha FROM usuarios_cadastrados WHERE email = $1', [email]);
+    const usuarioLogin = response.rows[0].email;
+    const senhaLogin = response.rows[0].senha;
+    console.log(response.rows)
+    bcrypt.compare(senha, senhaLogin, function(err, result){
+
+        if(usuarioLogin == email && result == true){
+            const idLogin = response.rows[0].id_usuario_cadastrado;
+            const usuarioInfo = {
+                "email": usuarioLogin,
+                "id": idLogin,
+                "admin": false
+            }
+
+            token = jwt.sign(usuarioInfo, 'abc123');
+            console.log(token);
+            res.cookie('token', token, { httpOnly: true});
+            res.json(response);
+        } else {
+            console.log('Usuario ou Senha incorretos');
+            res.send('Usuario ou Senha incorretos');
+        }
+    })
+}
 
 const deleleUsuario = async (req, res) => {
     const id = parseInt(req.params.id);
@@ -40,5 +72,6 @@ module.exports = {
     getUsuario,
     cadastrarUsuario,
     deleleUsuario,
-    alterarUsuario
+    alterarUsuario,
+    loginUsuario
 }
--- src/routes/usuariosRouter.js
+++ src/routes/usuariosRouter.js
@@ -2,9 +2,10 @@ const { Router } = require('express');
 const router = Router();
 
 
-const { getUsuario, cadastrarUsuario, deleleUsuario, alterarUsuario } = require('../controllers/usuariosController');
+const { getUsuario, cadastrarUsuario, deleleUsuario, alterarUsuario, loginUsuario } = require('../controllers/usuariosController');
 
 router.get('/Usuarios', getUsuario);
+router.post('/login', loginUsuario)
 router.post('/Usuarios', cadastrarUsuario);
 router.delete('/Usuarios/:id', deleleUsuario)
 router.put('/Usuarios/:id', alterarUsuario)
